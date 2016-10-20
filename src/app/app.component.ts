629dc16b64eb26a956fe9d73db91500e1f260249
Jason Goemaat <jason.goemaat@gmail.com>
2016-10-20 06:25:59 -0500

    datetime testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  mydate: string = "2016-12-25T00:00";
   title = 'app works!';
+
+  constructor() {
+    window['CAPP'] = this;
+  }
 }
